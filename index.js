ba28691c6c986dd6695762975b2c2838efafae98
ignathiooo <68048278+ignathiooo@users.noreply.github.com>
2021-11-16 20:38:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+    function add(){
+        const input1 = parseFloat(document.querySelector('#value1').value);
+        const input2 = parseFloat(document.querySelector('#value2').value);
+        const result = input1 + input2;
+        document.querySelector('#result').innerHTML = result;
+    }
+    document.querySelector('#ADD').addEventListener('click',add);
+    
+    function subtract(){
+        const input1 = document.querySelector('#value1').value;
+        const input2 = document.querySelector('#value2').value;
+        const result = input1 - input2;
+        document.querySelector('#result').innerHTML = result;
+    }
+    
+    document.querySelector('#SUBTRACT').addEventListener('click',subtract);
+    
+    function multiply(){
+        const input1 = parseFloat(document.querySelector('#value1').value);
+        const input2 = parseFloat(document.querySelector('#value2').value);
+        const result = input1 * input2;
+        document.querySelector('#result').innerHTML = result;
+    }
+    document.querySelector('#MULTIPLY').addEventListener('click',multiply);
+
+    function divide(){
+        const input1 = document.querySelector('#value1').value;
+        const input2 = document.querySelector('#value2').value;
+        const result = input1 / input2;
+        document.querySelector('#result').innerHTML = result;
+    }
+    document.querySelector('#DIVIDE').addEventListener('click',divide);
