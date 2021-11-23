436678f071ec32522327f6047d85078c5c05b4dd
ignathiooo <68048278+ignathiooo@users.noreply.github.com>
2021-11-23 16:09:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 
-    function add(){
+    function getData(){
         const input1 = parseFloat(document.querySelector('#value1').value);
         const input2 = parseFloat(document.querySelector('#value2').value);
-        const result = input1 + input2;
+
+        return{
+            first: input1,
+            second: input2,
+            firstValue: input1,
+            secondValue: input2,
+            isValid: input1 !== '' && input2 !== '',
+        };
+    }
+    function add(){
+        const data = getData()
+        if (data.isValid) {
+        const result = data.firstValue + data.secondValue;
         document.querySelector('#result').innerHTML = result;
         }
     }
     document.querySelector('#ADD').addEventListener('click',add);
     
     function subtract(){
-        const input1 = document.querySelector('#value1').value;
-        const input2 = document.querySelector('#value2').value;
-        const result = input1 - input2;
+        const data = getData()
+        if (data.isValid) {
+        const result = data.firstValue - data.secondValue;
         document.querySelector('#result').innerHTML = result;
+        }
     }
-    
     document.querySelector('#SUBTRACT').addEventListener('click',subtract);
     
     function multiply(){
-        const input1 = parseFloat(document.querySelector('#value1').value);
-        const input2 = parseFloat(document.querySelector('#value2').value);
-        const result = input1 * input2;
+        const data = getData()
+        if (data.isValid) {
+        const result = data.firstValue * data.secondValue;
         document.querySelector('#result').innerHTML = result;
+    }
     }
     document.querySelector('#MULTIPLY').addEventListener('click',multiply);
 
     function divide(){
-        const input1 = document.querySelector('#value1').value;
-        const input2 = document.querySelector('#value2').value;
-        const result = input1 / input2;
+        const data = getData()
+        if (data.isValid) {
+            const result = data.firstValue / data.secondValue; 
         document.querySelector('#result').innerHTML = result;
     }
+}
     document.querySelector('#DIVIDE').addEventListener('click',divide);
